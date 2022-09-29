f7cf452bfbb9129834e7cd91f8ae0e260406f85a
solidmaradini <solidmaradini@gmail.com>
2022-09-29 11:48:11 +0200

    temperature

---
+++ src/main/java/org/example/TemperatureConverter.java
@@ -0,0 +1,63 @@
+package org.example;
+
+public class TemperatureConverter {
+           public static double fahrenheitToCelsius(double fahrenheit) {
+
+            double celsius;
+            celsius = (fahrenheit - 32) / 1.8;
+            System.out.println("Temperature in celsius:" + celsius);
+            return celsius;
+        }
+
+        public static double fahrenheitToKelvin(double fahrenheit) {
+
+            double kelvin;
+            kelvin = ((273.15 * fahrenheit - 273.15) * 9 / 5 + 32) +32;
+            System.out.println("Temperature in kelvin:" + kelvin);
+            return kelvin;
+        }
+
+        public static double celsiusToFahrenheit(double celsius) {
+
+            double fahrenheit;
+            fahrenheit = celsius * 9 / 5 +32;
+            System.out.println("Temperature in fahrenheit:" + fahrenheit);
+            return fahrenheit;
+        }
+
+        public static double celsiusToKelvin(double celsius) {
+
+            double kelvin;
+            kelvin = celsius - 273;
+            System.out.println("Temperature in kelvin:" + kelvin);
+            return kelvin;
+        }
+
+        public static double kelvinToFarenheit(double kelvin) {
+
+            double fahrenheit;
+            fahrenheit = (-457.9 * kelvin - 32) * 5 / 9 +273.15;
+            System.out.println("Temperature in fahrenheit:" + fahrenheit);
+            return fahrenheit;
+        }
+
+        public static double kelvinToCelsius(double kelvin) {
+
+            double celsius;
+            celsius = kelvin + 273;
+            System.out.println("Temperature in celsius:" + celsius);
+            return celsius;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/TemperatureTest.java
+++ src/test/java/TemperatureTest.java
@@ -1,8 +1,34 @@
+import org.example.TemperatureConverter;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class TemperatureTest {
 
+    @Test
+    @DisplayName("Check that the temperature converted method works correctly.")
+    void temperatureConverter_OK() {
 
+        double t = 1;
+        assertEquals(t * 9 / 5 +32, TemperatureConverter.celsiusToFahrenheit(t));
+        assertEquals((t - 32) * 5 / 9, TemperatureConverter.fahrenheitToCelsius(t));
+        assertEquals(t - 273, TemperatureConverter.celsiusToKelvin(t));
+        assertEquals(((273.15 * t - 273.15) * 9 / 5 + 32) +32, TemperatureConverter.fahrenheitToKelvin(t));
+        assertEquals(t + 273, TemperatureConverter.kelvinToCelsius(t));
+        assertEquals((-457.9 * t - 32) * 5 / 9 +273.15, TemperatureConverter.kelvinToFarenheit(t));
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
